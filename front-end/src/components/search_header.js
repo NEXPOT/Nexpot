f4b7b345d8713d3c800b63eec36b70c0fa460863
oduodg <werewildfire@gmail.com>
2022-08-23 17:02:34 +0900

    main page youtube video list-up

--- front-end/src/components/search_header.js
+++ front-end/src/components/search_header.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import './search_header.module.css';
 import { Search, ArrowRepeat } from 'akar-icons';
-
 
 const SearchHeader = () => {
 	return (
@@ -10,14 +8,14 @@ const SearchHeader = () => {
 				<div className='mx-10 py-3 flex justify-between self-center'>
 					<span className="text-white">NEXPOT</span>
 					<div>
-					<button className="mx-2 px-4 py-1 inline-flex rounded-lg border border-stone-400 text-stone-400 text-xs font-medium">
-						<Search strokeWidth={2} size={15} className="mr-2" />
-						검색
-					</button>
-					<button className="mx-2 px-4 py-1 inline-flex rounded-lg border border-blue-600 text-blue-600 text-xs font-medium">
-					<ArrowRepeat strokeWidth={2} size={15} className="mr-2" />	
-						번역
-					</button>
+						<button className="mx-2 px-4 py-1 inline-flex rounded-lg border border-stone-400 text-stone-400 text-xs font-medium">
+							<Search strokeWidth={2} size={15} className="mr-2" />
+							검색
+						</button>
+						<button className="mx-2 px-4 py-1 inline-flex rounded-lg border border-blue-600 text-blue-600 text-xs font-medium">
+							<ArrowRepeat strokeWidth={2} size={15} className="mr-2" />
+							번역
+						</button>
 					</div>
 				</div>
 			</div>
---
+++ front-end/src/components/video_list.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const VideoList = ({ regionName }) => {
+	const [video, setVideo] = useState([]);
+
+	/** ~으로 떠나볼까요? */
+	let regionTitle;
+	if (regionName === "서울" || regionName === "대구" || regionName === "광주" || regionName === "경기") {
+		regionTitle = "로 떠나볼까요?";
+	} else {
+		regionTitle = "으로 떠나볼까요?";
+	}
+
+	const region = {
+		"서울": "seoul",
+		"부산": "busan",
+		"인천": "incheon"
+	}
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const res = await axios.get("http://127.0.0.1:8000/api/youtube", {
+					params: {
+						region: region[regionName],
+					},
+				});
+				const _video = await res.data.map(item => ({
+					title: item.title,
+					thumbnail: item.thumbnail
+				}))
+				setVideo(video.concat(_video))
+			} catch (e) {
+				console.error(e.message);
+			}
+		}
+		getData();
+	}, [])
+
+	//console.log(video);
+
+	return (
+		<>
+			<div className="mx-10 mt-8 text-white">
+				<p className="mb-5 text-2xl font-bold">{regionName}
+					<span className="text-[#c4c4c4]">{regionTitle}
+					</span>
+				</p>
+				
+				<div className='flex overflow-scroll'>
+
+				{video.map((item, idx) =>
+				 
+					<div className="inline mr-6" key={idx}>
+						<img alt="thumbnail" src={item.thumbnail} />
+						<p className='text-sm w-80'>{item.title}</p>
+					</div>
+				
+				)}
+				</div>
+				
+			</div>
+		</>
+	);
+};
+
+export default VideoList;

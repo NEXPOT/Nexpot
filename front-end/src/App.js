0b18bc300d1a38f0611e149dbde911cb4befceef
oduodg <werewildfire@gmail.com>
2022-08-28 16:35:38 +0900

    fix scroll location when navigates a page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,12 @@ import SearchHeader from './components/search_header';
 import Footer from './components/footer';
 import { Main, Detail, SearchResult } from './pages';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from './components/scroll_to_top';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <div className="wrapper">
         <div className="contentWrapper">
           <SearchHeader />

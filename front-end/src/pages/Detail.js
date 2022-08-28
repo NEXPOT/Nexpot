240a57bac949971d24fe14a4e3a0013df8744178
naro-Kim <gracias9022@gmail.com>
2022-08-29 01:26:00 +0900

    [FEAT] : Detail.js 그라디언트, 텍스트css, 여행 코스, 지도

--- front-end/src/pages/Detail.js
+++ front-end/src/pages/Detail.js
@@ -1,27 +1,88 @@
-import React from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation, useParams } from "react-router-dom";
+import Map from "../components/map";
 
 export default function Detail() {
-	let { videoid } = useParams();
-	const location = useLocation();
-	console.log("videoid: " + videoid);
-	console.log(location);
+  const location = useLocation();
+  let { videoid } = useParams();
+  const [detail, setDetail] = useState([]);
+  const [place, setPlaces] = useState([]);
 
-	return (
-		<div className='mx-10'>
-			<img className='m-auto' alt="thumbnail" src={location.state.thumbnail} />
-			{/* TODO: 썸네일 그라디언트 */}
+  // 첫 렌더링에 videoid로 상세 정보를 가져옵니다.
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get(
+          "http://13.209.16.143:3306/api/youtube/" + videoid
+        );
+        const _detail = [
+          {
+            videoid: res.data.videoid,
+            title: res.data.title,
+            channelname: res.data.channelname,
+            views: res.data.views,
+          },
+        ];
+        setDetail(_detail);
+        setPlaces(res.data.places);
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+    getData();
+  }, []);
 
-			<div className='mx-10 text-white'>
-				<p className='mt-24 text-5xl font-bold truncate'>{location.state.title}</p>
-				<p className='mt-6 text-sm font-bold'>{location.state.channelname}</p>
-			</div>
-			<div className='text-white'>
-				<p className='mx-5 text-base font-bold mt-28'>관광코스</p>
-				<p className='mt-6 text-sm text-[#808080] font-bold'>해동용궁사 스카이라인 루지 코스를 적어봅시다. 코스A 코스B 스테이크 요리 </p>
-				<p className='mx-5 mt-6 text-base font-bold'>여행지 정보</p>
-				<p className='mx-5 mt-6 text-base font-bold'>상세 정보</p>
-			</div>
-		</div>
-	)
+  // useEffect를 통해, onClickPlace 액션이 일어나면 Map 컴포넌트의 마커 위치를 새로 렌더링합니다...?
+  useEffect(() => {}, []);
+
+  const onClickPlace = (e) => {
+    console.log(e.target);
+    console.log(e.target.state);
+  };
+
+  const setPlaceItem = () => {
+    return place.map((item, idx) => (
+      <div>
+        <button
+          onClick={onClickPlace}
+          className="mx-10 mt-6 text-xs font-normal"
+          key={idx}
+          state={item.px}
+        >
+          {item.pname}
+        </button>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="mx-56 text-white">
+      <div className="videoWrapper">
+        <div className="img-gr"></div>
+        <img
+          className="videoImg m-auto"
+          alt="thumbnail"
+          src={location.state.thumbnail}
+        />
+      </div>
+      {/** To-Do 이미지 위 텍스트 오버레이  */}
+      <div className="overlay mx-10">
+        <p className="channel mt-6 text-xl font-bold">
+          {location.state.channelname}
+        </p>
+        <p className="title mt-2 text-sm font-normal">{location.state.title}</p>
+        <button className="btn mt-8 px-40 py-2 text-sm font-medium rounded-lg bg-slate-50 text-slate-800">
+          영상 재생
+        </button>
+      </div>
+      <div>
+        <p className="mx-10 text-base font-bold mt-28">관광코스</p>
+        <p className="mx-10 mt-6 text-base font-bold">여행지 정보</p>
+        <div className="flex col-auto">{place && setPlaceItem()}</div>
+        <Map place={place} />
+        <p className="mx-10 mt-6 text-base font-bold">상세 정보</p>
+      </div>
+    </div>
+  );
 }

9149924b2ad7fb9b584aaec8a4530f2b5135feda
oduodg <werewildfire@gmail.com>
2022-12-05 13:38:06 +0900

    💄 Edit footer

--- front-end/src/components/footer.js
+++ front-end/src/components/footer.js
@@ -8,29 +8,20 @@ export default function footer() {
 			</div>
 			<div className='flex flex-col sm:flex-row'>
 				<div className='flex-none pt-4 pl-10 font-normal'>
-					<span className='block text-[#525959] text-xs'>PM</span>
+					<span className='block text-[#525959] text-xs'>PM/FE</span>
 					<span className='text-sm md:text-base'>Eun Lee</span>
 				</div>
 				<div className='flex-none pt-4 pl-10 font-normal'>
-					<span className='block text-[#525959] text-xs'>Design</span>
+					<span className='block text-[#525959] text-xs'>Design/FE</span>
 					<span className='text-sm md:text-base'>Ahhyun Kim</span>
 				</div>
 				<div className='flex-none pt-4 pl-10 font-normal'>
-					<span className='block text-[#525959] text-xs'>Frontend</span>
-					<span className='text-sm md:text-base'>Eun Lee</span><br />
-					<span className='text-sm md:text-base'>Ahhyun Kim</span>
-				</div>
-				<div className='flex-none pt-4 pl-10 font-normal'>
-					<span className='block text-[#525959] text-xs'>Backend</span>
+					<span className='block text-[#525959] text-xs'>BE</span>
 					<span className='text-sm md:text-base'>Jihoo Kim</span>
 				</div>
 				<div className='flex-none pt-4 pl-10 font-normal'>
 					<span className='block text-[#525959] text-xs'>DB/NLP</span>
 					<span className='text-sm md:text-base'>Jiseok Kim</span>
-				</div>
-				<div className='flex-none pt-4 pl-10 font-normal'>
-					<span className='block text-[#525959] text-xs'>Marketing</span>
-					<span className='text-sm md:text-base'>Seoyun Oh</span>
 				</div>
 
 				<span className='flex-auto px-10 mb-4 text-sm font-medium text-end pt-7 sm:mb-0 md:text-lg'>2022 TourApi x KaKao</span>
